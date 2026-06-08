1b3cadaeb0481632c1c63fc52ff937c528f6a165
= <piyushyenorkar1@gmail.com>
2026-06-08 18:30:36 +0530

    style: shift cursive signature further down and increase wave height again

--- combine.py
+++ combine.py
@@ -1,8 +1,8 @@
 import urllib.request
 import re
 
-capsule_url = "https://capsule-render.vercel.app/api?type=waving&color=0:1b5e20,100:81c784&height=260&section=header"
-typing_url = "https://readme-typing-svg.demolab.com?font=Great+Vibes&weight=400&size=85&duration=1&pause=100000&color=FFFFFF&center=true&vCenter=true&repeat=false&width=1000&height=260&lines=Piyush+Yenorkar"
+capsule_url = "https://capsule-render.vercel.app/api?type=waving&color=0:1b5e20,100:81c784&height=300&section=header"
+typing_url = "https://readme-typing-svg.demolab.com?font=Great+Vibes&weight=400&size=85&duration=1&pause=100000&color=FFFFFF&center=true&vCenter=true&repeat=false&width=1000&height=300&lines=Piyush+Yenorkar"
 
 req1 = urllib.request.Request(capsule_url, headers={'User-Agent': 'Mozilla/5.0'})
 capsule_svg = urllib.request.urlopen(req1).read().decode('utf-8')
@@ -18,8 +18,8 @@ style_block = "\n".join(styles)
 inner_typing = re.sub(r'</?svg[^>]*>', '', typing_svg)
 
 # Find the closing </svg> tag in capsule_svg and insert the typing elements before it
-# Shift heading down by 20 pixels
-combined_svg = capsule_svg.replace('</svg>', f'\n{style_block}\n<g transform="translate(0, 20)">\n{inner_typing}\n</g>\n</svg>')
+# Shift heading down by 40 pixels
+combined_svg = capsule_svg.replace('</svg>', f'\n{style_block}\n<g transform="translate(0, 40)">\n{inner_typing}\n</g>\n</svg>')
 
 with open('c:/Users/piyus/Downloads/piyushyenorkar-github/assets/header_combined.svg', 'w', encoding='utf-8') as f:
     f.write(combined_svg)
